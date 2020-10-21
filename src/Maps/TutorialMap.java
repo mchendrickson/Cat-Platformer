f796f06a3b536c41f9ab724b9f6d5d6546505bbb
mchrpt <mchrpt@yahoo.com>
2020-10-21 12:27:17 -0400

    Added a map

---
+++ src/Maps/TutorialMap.java
@@ -0,0 +1,15 @@
+package Maps;
+
+import Level.Map;
+import Level.Tileset;
+import Tilesets.CommonTileset;
+import Utils.Point;
+
+public class TutorialMap extends Map{
+
+	public TutorialMap() {
+		super("tutorial_map.txt", new CommonTileset(), new Point(1, 11));
+		// TODO Auto-generated constructor stub
+	}
+
+}
