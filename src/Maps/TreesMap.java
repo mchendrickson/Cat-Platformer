744923a6d6b824dc2b76c10b2f5a94d090f0989c
mchrpt <mchrpt@yahoo.com>
2020-11-04 16:05:55 -0500

    Added the Tree map

--- src/Maps/TreesMap.java
+++ src/Maps/TreesMap.java
@@ -1,12 +1,27 @@
 package Maps;
 
+import java.util.ArrayList;
+
+import EnhancedMapTiles.EndLevelBox;
+import Level.EnhancedMapTile;
 import Level.Map;
 import Tilesets.CommonTileset;
 import Utils.Point;
 
 public class TreesMap extends Map{
 	public TreesMap() {
-		super("trees_map.txt", new CommonTileset(), new Point(1, 9));
+		super("trees_map.txt", new CommonTileset(), new Point(1, 24));
 		// TODO Auto-generated constructor stub
 	}
+	
+	@Override
+    public ArrayList<EnhancedMapTile> loadEnhancedMapTiles() {
+        ArrayList<EnhancedMapTile> enhancedMapTiles = new ArrayList<>();
+
+        enhancedMapTiles.add(new EndLevelBox(
+                getPositionByTileIndex(134, 1)
+        ));
+
+        return enhancedMapTiles;
+    }
 }
